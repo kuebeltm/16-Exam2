2e80cbb1449135b3822313061794c4e61614c78b
Todd Kuebelbeck <kuebeltm@rose-hulman.edu>
2018-04-17 21:33:51 -0400

    Committed by Todd

--- src/problem1.py
+++ src/problem1.py
@@ -2,8 +2,8 @@
 Exam 2, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  April 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Todd Kuebelbeck.  April 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -12,7 +12,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the code of the  Rect  class below.
+# DONE: 2.  READ the code of the  Rect  class below.
 #
 #   Once you are confident that you understand the  Rect  class and its code,
 #   change the TO-DO for this problem to DONE.
@@ -29,13 +29,22 @@ class Rect(object):
 def run_test_problem1():
     """ Tests the   problem1   function. """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement at least 2 tests of the  problem1  function.
+    # DONE: 3. Implement at least 2 tests of the  problem1  function.
     # -------------------------------------------------------------------------
     print()
     print('--------------------------------------------------')
     print('Testing the   problem1   function:')
     print('--------------------------------------------------')
 
+    rectangles = [Rect(3,4), Rect(4,5), Rect(5,6)]
+    print('Expected: ', 62)
+    print('Received: ', problem1(rectangles))
+    print('---')
+
+    rectangles = [Rect(9, 8), Rect(8, 7), Rect(7, 6)]
+    print('Expected: ', 170)
+    print('Received: ', problem1(rectangles))
+    print('---')
 
 def problem1(rectangles):
     """
@@ -53,8 +62,13 @@ def problem1(rectangles):
     :return: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     # -------------------------------------------------------------------------
+    area_sum = 0
+
+    for k in range(len(rectangles)):
+        area_sum = area_sum + (rectangles[k].w * rectangles[k].h)
+    return area_sum
 
 
 # -----------------------------------------------------------------------------
